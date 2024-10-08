49deeb804790b32723e38dd42488e109d862a830
Phatthawut <42006909+Phatthawut@users.noreply.github.com>
2024-10-08 11:51:47 +0700

    fix divider position 2

--- src/components/ServiceBanner.jsx
+++ src/components/ServiceBanner.jsx
@@ -10,22 +10,22 @@ function BandnerSection ({bannerName}) {
     return (
         <div className="relative w-full">
         <div className="relative flex flex-col items-center justify-evenly bg-blue-700 h-[100vh] text-white font-bold">
-            <div className="w-full ">
-            <div className="divider w-4/5 mx-auto bg-white h-0.5"></div>
-            <h2 className="font-Domine text-3xl md:text-4xl text-center mx-auto w-1/2 md:w-1/3 lg:hidden">( K2 Venture Creative Comunication Agency )</h2>
-            <div className="lg:flex w-full px-28 justify-between font-Domine text-lg hidden">
-                <div>( K2 Venture</div>
-                <div>Creative Comunication Agency)</div>
+            <div className="md:w-4/5 -mt-8">
+                <div className="divider mx-auto bg-white h-0.5"></div>
+                <h2 className="font-Domine text-3xl md:text-4xl text-center mx-auto w-1/2 md:w-[10em] lg:hidden">( K2 Venture Creative Comunication Agency )</h2>
+                <div className="lg:flex justify-between font-Domine text-lg hidden">
+                    <div>( K2 Venture</div>
+                    <div>Creative Comunication Agency)</div>
+                </div>
             </div>
-            </div>
-            <div className="text-7xl md:text-8xl w-full pl-12 md:pl-32">
+            <div className="text-6xl md:text-7xl lg:text-8xl w-full pl-12 md:pl-32">
                 <h3 className="font-Arapey italic font-medium">{serviceData.title}</h3>
-                <h3 className=" -mt-6 md:-mt-8 font-normal uppercase">{serviceData.title2}</h3>
+                <h3 className=" -mt-6 md:-mt-7 lg:-mt-9 font-normal uppercase">{serviceData.title2}</h3>
             </div>
             
-            <div className="w-full text-5xl md:text-7xl lg:text-8xl pl-12 md:pl-32 uppercase">
+            <div className="w-full text-6xl md:text-8xl lg:text-9xl pl-12 md:pl-32 uppercase">
                 <h3 className="font-Anek font-light">{firstPart}</h3>
-                <h3 className="font-semibold -mt-2 lg:-mt-7">{secondPart}</h3>
+                <h3 className="font-semibold -mt-4 md:-mt-6 lg:-mt-7">{secondPart}</h3>
             </div>
             
         </div>
